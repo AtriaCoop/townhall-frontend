dccab959ce918cdefc0b73fbca5695a37b52cf51
JordanBandur <jordanbandur@hotmail.ca>
2024-09-10 23:57:33 -0600

    Configure Jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '\\.(css|scss|sass)$': 'identity-obj-proxy',
   },
   moduleDirectories: ['node_modules', 'src'],
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transform: {
+    '^.+\\.[jt]sx?$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(axios)/)', '\\.pnp\\.[^\\/]+$'],
 };
