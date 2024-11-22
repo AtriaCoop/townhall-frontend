7c47412d1fc31d17d39d5af3435d86f68a4a8fac
Ryan <ryeean16@gmail.com>
2024-11-22 01:08:40 -0800

    banner test

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,12 @@
 import Navigation from '../components/navigation/navigation.jsx'
 import Footer from '../components/footer/footer.jsx'
+import Header from '@/components/header/header.jsx';
 
 export default function Home() {
   return (
     <div className="home">
         <Navigation />
+        <Header />
         <Footer />
     </div>
   );
