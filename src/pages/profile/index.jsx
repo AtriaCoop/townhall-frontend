ad613665a77ac38eef64ccc117a5ac069aac39d6
Ryan <ryeean16@gmail.com>
2025-01-27 17:03:11 -0800

    removed organization dashboard component from profile

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,11 +1,9 @@
-import OrganizationDashboard from '@/components/organizationDashboard/organizationDashboard.jsx';
 import Navigation from '../../components/navigation/navigation.jsx';
 
 export default function Profile() {
     return (
         <div className="profile">
             <Navigation />
-            <OrganizationDashboard />
         </div>
     )
 }
