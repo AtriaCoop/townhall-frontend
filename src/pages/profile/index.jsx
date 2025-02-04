0e34c3ad624458aa00295abc37a8b047f2761e1e
Ryan <ryeean16@gmail.com>
2025-02-03 16:15:34 -0800

    profile page first draft

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,10 +1,11 @@
 import Navigation from '../../components/navigation/navigation.jsx';
-import Footer from '../../components/footer/footer.jsx';
+import ProfilePage from '@/components/profilePage/profilePage.jsx';
 
 export default function Profile() {
     return (
         <div className="profile">
             <Navigation />
+            <ProfilePage />
         </div>
     )
 }
